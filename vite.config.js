7e359faf27cbd9c02fa19f06e5af99c621edc81a
kindtiger <seonbeom2@gmail.com>
2024-12-06 15:50:00 +0900

    build

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,9 @@ import { resolve } from "node:path";
         main: resolve(__dirname,'index.html'),
         product: resolve(__dirname,'src/pages/product/index.html'),
         login: resolve(__dirname,'src/pages/login/index.html'),
+        detail: resolve(__dirname,'src/pages/detail/index.html'),
+        newPost: resolve(__dirname,'src/pages/newPost/index.html'),
+        register: resolve(__dirname,'src/pages/register/index.html'),
       }
     }
   }
